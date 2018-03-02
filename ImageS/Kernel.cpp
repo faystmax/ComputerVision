06332494ee2bdfa170b06eb9cb95384dce69a296
maximus <faystmax@gmail.com>
2018-03-03 01:04:52 +0700

    fix Kernel constructor

--- ImageS/Kernel.cpp
+++ ImageS/Kernel.cpp
@@ -1,10 +1,10 @@
 #include "Kernel.h"
 #include <iostream>
 
-Kernel::Kernel(int width, int height, vector<double> core) {
+Kernel::Kernel(int width, int height, vector<double> core)
+    :core(core) {
     this->width = width;
     this->height = height;
-    this->core = core;
 }
 
 void Kernel::rotate() {
