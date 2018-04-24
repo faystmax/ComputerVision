dd11b20574cfac9b7b9d903a1e34937e0a7c973d
maximus <faystmax@gmail.com>
2018-04-25 00:28:03 +0700

    need fixes 9

--- ImageS/Hough.cpp
+++ ImageS/Hough.cpp
@@ -7,11 +7,12 @@ Transform Hough::search(vector<Vector> &lines, const Image &img1, const Image &i
 
     int width = img1.getWidth() / 50;
     int height = img1.getHeight() / 50;
-    int size = 10; // TODO
+    int sizeMax = 10;
     int angle = 360/30;
     double angleStep = 2 * M_PI / angle;
+    double sizeStep = 2 / sizeMax;
 
-    double parameterSpace[width][height][size][angle];      /* Пространство параметров */
+    double parameterSpace[width][height][sizeMax][angle] = {0};      /* Пространство параметров */
 
     for (size_t i = 0; i < lines.size(); i++) {
         double diff_angle = lines[i].second.getOrientation() - lines[i].first.getOrientation();
@@ -23,15 +24,23 @@ Transform Hough::search(vector<Vector> &lines, const Image &img1, const Image &i
         double diff_x = distance * cos(main_angle);
         double diff_y = distance * sin(main_angle);
 
-        int index_X = (lines[i].second.getInterPoint().x -  diff_x) / 50;
-        int index_Y = (lines[i].second.getInterPoint().y -  diff_y) / 50;
-        int index_Size = size; // TODO
+        int index_X = (lines[i].second.getInterPoint().x +  diff_x) / 50;
+        int index_Y = (lines[i].second.getInterPoint().y +  diff_y) / 50;
+        int index_Size = size / sizeStep; // TODO
         int index_Angle = diff_angle / angleStep;
 
-        // Кладём голос
-        parameterSpace[index_X][index_Y][index_Size][index_Angle]++;
-    }
 
+        // Интерполируем (размазываем)
+        for (int m1=-1; m1 <= 1; m1++)
+            for (int m2=-1; m2 <= 1; m2++)
+                for (int m3=-1; m3 <= 1; m3++)
+                    for (int m4=-1; m4 <= 1; m4++) {
+                        parameterSpace[(index_X + m1 + width) % width]
+                        [(index_Y + m2 + height) % height]
+                        [(index_Size + m3 + sizeMax) % sizeMax]
+                        [(index_Angle + m4 + angle) % angle]++;
+                    }
+    }
 
     double x, y, bestSize, bestAngle;
     int bestVotes = 0;
@@ -39,13 +48,13 @@ Transform Hough::search(vector<Vector> &lines, const Image &img1, const Image &i
     // Ищем максимальный
     for (int i = 1; i < width - 1; i++) {
         for (int j = 1; j < height - 1; j++) {
-            for (int k = 0; k < size - 1; k++) {
+            for (int k = 0; k < sizeMax - 1; k++) {
                 for (int n = 0; n < angle - 1; n++) {
                     if (parameterSpace[i][j][k][n] > bestVotes) {
                         x = i;
                         y = j;
-                        bestAngle = k;
-                        angle = n;
+                        bestSize = k;
+                        bestAngle = n;
                         bestVotes = parameterSpace[i][j][k][n];
                     }
                 }
@@ -53,11 +62,11 @@ Transform Hough::search(vector<Vector> &lines, const Image &img1, const Image &i
         }
     }
 
-    Transform transform(50 * x + 25, 50 * y + 25,  bestSize, bestAngle, img1.getWidth() * bestSize, img1.getHeight() * bestSize);
+    Transform transform(50 * x + 25, 50 * y + 25,  bestSize, bestAngle* angleStep + angleStep/2, img1.getWidth() * bestSize, img1.getHeight() * bestSize);
     return transform;
 }
 
-void Hough::calcCenterDistanceAndAngle(vector<Descriptor>& desc, const Image &image) {
+void Hough::calcCenterDistanceAndAngle(vector<Descriptor> &desc, const Image &image) {
     double x_center = image.getWidth()/2.;
     double y_center = image.getHeight()/2.;
 
